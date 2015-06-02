22f7c82793cd9d8e6c735f3b577c28e36b763705
Aesmade <aesmade@gmail.com>
2015-06-03 01:26:39 +0300

    Initial

---
+++ config/config.php
@@ -0,0 +1,41 @@
+<?php
+/* ========================================================
+ * OpeneClass 2.2 configuration file
+ * Automatically created by install on 2015-05-23 22:12
+ * ======================================================== */
+
+$urlServer = "http://alitez.compsec/";
+$urlAppend = "";
+$webDir    = "/home/Alitez/www/" ;
+
+$mysqlServer = "localhost";
+$mysqlUser = "Alitez";
+$mysqlPassword = "v6ChDMzBXAKn";
+$mysqlMainDb = "Alitez";
+$phpMyAdminURL = "../admin/mysql/";
+$phpSysInfoURL = "../admin/sysinfo/";
+$emailAdministrator = "webmaster@localhost";
+$administratorName = "Διαχειριστής";
+$administratorSurname = "Πλατφόρμας";
+$siteName = "Open eClass";
+
+$telephone = "+30 2xx xxxx xxx";
+$fax = "";
+$emailhelpdesk = "";
+
+$language = "greek";
+
+$Institution = "Ακαδημαϊκό Διαδίκτυο GUNet ";
+$InstitutionUrl = "http://www.gunet.gr/";
+$postaddress = "";
+
+$have_latex = FALSE;
+$close_user_registration = FALSE;
+
+$persoIsActive = TRUE;
+$durationAccount = "126144000";
+
+define("UTF8", true);
+
+
+$encryptedPasswd = true;
